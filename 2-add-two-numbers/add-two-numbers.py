971438bc3ab62e53d10248996881b70667d1d863
Abhinav Sahoo <sahooabhinav5@gmail.com>
2026-07-22 08:54:49 +0530

    Time: 7 ms (24.29%) | Memory: 19.4 MB (40.43%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,52 +5,81 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # a=l1
+        # b=l2
+        # start=None
+        # temp=start
+        # carry=0
+        # while a!=None and b!=None:
+        #     value=a.val+b.val+carry
+        #     term=value%10
+        #     carry=value//10
+        #     node=ListNode(term)
+        #     if start==None:
+        #         start=node
+        #         temp=node
+        #     else:
+        #         temp.next=node
+        #         temp=node
+        #     a=a.next
+        #     b=b.next
+        # while a!=None:
+        #     value=a.val+carry
+        #     term=value%10
+        #     carry=value//10
+        #     node=ListNode(term)
+        #     if start==None:
+        #         start=node
+        #         temp=node
+        #     else:
+        #         temp.next=node
+        #         temp=node
+        #     a=a.next
+        # while b!=None:
+        #     value=b.val+carry
+        #     term=value%10
+        #     carry=value//10
+        #     node=ListNode(term)
+        #     if start==None:
+        #         start=node
+        #         temp=node
+        #     else:
+        #         temp.next=node
+        #         temp=node
+        #     b=b.next
+        # if carry!=0:
+        #     node=ListNode(carry)
+        #     temp.next=node
+        # return start
+
+
+
         a=l1
         b=l2
-        start=None
-        temp=start
+
+        result=head=ListNode(0)
         carry=0
-        while a!=None and b!=None:
-            value=a.val+b.val+carry
-            term=value%10
-            carry=value//10
-            node=ListNode(term)
-            if start==None:
-                start=node
-                temp=node
+
+        while a or b:
+            s=0
+            if a:
+                s+=a.val
+                a=a.next
+            if b:
+                s+=b.val
+                b=b.next
+            s+=carry
+            if s>9:
+                s=s%10
+                carry=1
             else:
-                temp.next=node
-                temp=node
-            a=a.next
-            b=b.next
-        while a!=None:
-            value=a.val+carry
-            term=value%10
-            carry=value//10
-            node=ListNode(term)
-            if start==None:
-                start=node
-                temp=node
-            else:
-                temp.next=node
-                temp=node
-            a=a.next
-        while b!=None:
-            value=b.val+carry
-            term=value%10
-            carry=value//10
-            node=ListNode(term)
-            if start==None:
-                start=node
-                temp=node
-            else:
-                temp.next=node
-                temp=node
-            b=b.next
-        if carry!=0:
-            node=ListNode(carry)
-            temp.next=node
-        return start
+                carry=0
+            head.next=ListNode(s)
+            head=head.next
+
+            if carry>0:
+                head.next=ListNode(carry)
+        return result.next
         
 
 
