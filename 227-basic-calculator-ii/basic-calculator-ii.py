8fa4912a1060a9435151e65a08709a939f858f0e
Abhinav Sahoo <sahooabhinav5@gmail.com>
2026-07-28 08:38:44 +0530

    Time: 59 ms (47.39%) | Memory: 22.3 MB (62.84%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack=[]
+        num=0
+        op="+"
+        for i in range(len(s)):
+            ch=s[i]
+            if ch.isdigit():
+                num=num*10+int(ch)
+            if (not ch.isdigit() and ch!=" ") or i==len(s)-1:
+                if op=="+":
+                    stack.append(num)
+                elif op=="-":
+                    stack.append(-num)
+                elif op=="*":
+                    stack.append(stack.pop()*num)
+                elif op=="/":
+                    stack.append(int(stack.pop()/num))
+                op=ch
+                num=0
+        return sum(stack)
