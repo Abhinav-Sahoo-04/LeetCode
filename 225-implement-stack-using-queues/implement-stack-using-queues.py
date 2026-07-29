1dc5fec613e6ba086b829f186007b6823355fedc
Abhinav Sahoo <sahooabhinav5@gmail.com>
2026-07-29 09:34:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (51.46%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -1,27 +1,31 @@
 class MyStack:
 
     def __init__(self):
-        self.q1 = deque()
-        self.q2 = deque()
+        self.stack=[]
+        self.t=-1
+        
+        
 
     def push(self, x: int) -> None:
-        self.q2.append(x)
+        self.stack.append(x)
+        self.t+=1
 
-        while self.q1:
-            self.q2.append(self.q1.popleft())
-        self.q1, self.q2 = self.q2, self.q1
         
 
     def pop(self) -> int:
-        return self.q1.popleft()
+        if self.t==-1:
+            return "Underflow"
+        val=self.stack.pop()
+        self.t-=1
+        return val
         
 
     def top(self) -> int:
-        return self.q1[0]
+        return self.stack[self.t]
         
 
     def empty(self) -> bool:
-        return not self.q1
+        return False if  self.stack else True
         
 
 
