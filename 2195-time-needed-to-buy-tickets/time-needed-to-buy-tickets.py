dfa2caf047475ec266f4c6a855b0b4810d47cae7
Abhinav Sahoo <sahooabhinav5@gmail.com>
2026-07-29 08:45:02 +0530

    Time: 8 ms (45.52%) | Memory: 19.2 MB (62.27%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        n = len(tickets)
+        time = 0
+        i = 0
+        while tickets[k] > 0:
+            if tickets[i] > 0:
+                tickets[i] -= 1
+                time += 1
+            i = (i + 1) % n
+        return time
