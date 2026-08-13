811d3e14507ce4544dc4bb9091e4ede9d4a3fda4
Abhinav Sahoo <sahooabhinav5@gmail.com>
2026-08-13 19:36:32 +0530

    Time: 11 ms (10.90%) | Memory: 20.3 MB (85.49%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,12 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit=0
-        mini=prices[0]
-        for i in range(1,len(prices)):
-            if prices[i]-mini>0:
-                profit+=prices[i]-mini
-                mini=prices[i]
-            else:
-                mini=min(mini,prices[i])
-        return profit
+        def BuySellStock(prices):
+            i=len(prices)
+            dp=[0]*2
+            prev=dp[:]
+            for a in range(i-1,-1,-1):
+                for b in range(1,-1,-1):
+                    if b==1:
+                        dp[b]=max(-prices[a]+prev[0],prev[1])
+                    else:
+                        dp[b] = max(prices[a] + prev[1], prev[0])
+                prev=dp[:]
+            return dp[1]
+        return BuySellStock(prices)
 
