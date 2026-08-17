fcf7bc5bdb3edd11d10394d809847fd42e16217b
Abhinav Sahoo <sahooabhinav5@gmail.com>
2026-08-17 20:15:36 +0530

    Time: 39 ms (52.55%) | Memory: 19.4 MB (69.98%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        def BuySellStock(prices,k):
+            i=len(prices)
+            dp=[[0]*(k+1) for _ in range(2)]
+            for b in range(2):
+                dp[b][0]=0
+            for a in range(2):
+                for b in range(k+1):
+                    dp[a][b]=0
+            prev=[row[:] for row in dp]
+            for a in range(i-1,-1,-1):
+                for b in range(2):
+                    for c in range(1,k+1):
+                        if b==1:
+                            profit=max(-prices[a]+prev[0][c],prev[1][c])
+                        else:
+                            profit = max(prices[a]+prev[1][c-1], prev[0][c])
+                        dp[b][c]=profit
+                prev = [row[:] for row in dp]
+            return dp[1][k]
+        return BuySellStock(prices,k)
+            
