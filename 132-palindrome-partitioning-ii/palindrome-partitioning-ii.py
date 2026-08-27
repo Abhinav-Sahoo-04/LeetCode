313aac367f26718d31146861086f34a8505f84fe
Abhinav Sahoo <sahooabhinav5@gmail.com>
2026-08-27 20:18:58 +0530

    Time: 2508 ms (13.81%) | Memory: 19.4 MB (89.12%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minCut(self, s: str) -> int:
+        n=len(s)
+        dp=[0]*(n+1)
+        for i in range(n-1,-1,-1):
+            mini=float("inf")
+            temp=""
+            for k in range(i,n):
+                temp+=s[k]
+                if temp==temp[::-1]:
+                    cost=1+dp[k+1]
+                    mini=min(cost,mini)
+            dp[i]=mini
+        return dp[0]-1
+        
